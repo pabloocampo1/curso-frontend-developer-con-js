46ad276e2163c22d7d04f0fed4b7642f5a4e9cc6
Pablo Ocampo <119111156+pabloocampo1@users.noreply.github.com>
2023-05-26 19:44:35 -0500

    se agrego al archivo index el codigo html de la parte principal del proyecto, y se4 agrego un menu para desktop

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuDestok = document.querySelector('.desktop-menu');
+const navbarEmail = document.querySelector('.navbar-email');
+
+navbarEmail.addEventListener("click", function() {
+   menuDestok.classList.toggle('inactive');
+  });
