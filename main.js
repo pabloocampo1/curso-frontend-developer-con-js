96d19fb288f6c75c801b71e361b5a3781c02789b
Pablo Ocampo <119111156+pabloocampo1@users.noreply.github.com>
2023-05-29 12:18:51 -0500

    se agrego el menu mobile al index, y los estilos al archivo style, con js se manipulo el DOM para que aparezca con un click el menu mobile, y desaparezca cuando este en modo destok

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+//menu destok
+
 const menuDestok = document.querySelector('.desktop-menu');
 const navbarEmail = document.querySelector('.navbar-email');
 
 navbarEmail.addEventListener("click", function() {
    menuDestok.classList.toggle('inactive');
   });
+
+ //menu mobile
+
+ const menuMobile = document.querySelector('.mobile-menu');
+ const logoMenuMobile = document.querySelector('.menuLogo');
+
+
+ logoMenuMobile.addEventListener("click", function() {
+   menuMobile.classList.toggle('inactive');
+ });
