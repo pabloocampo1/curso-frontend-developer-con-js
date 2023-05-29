2b5445ff714bf206c7edc4ad48134cc58e16c73c
Pablo Ocampo <119111156+pabloocampo1@users.noreply.github.com>
2023-05-29 13:34:19 -0500

    se agrego la parte visual de los productos seleccionados y su estilo css, tambien se agrego una funcionalidad en main.js para cerrar una pantalla mientras abrimos otra.

--- main.js
+++ main.js
@@ -5,15 +5,44 @@ const menuDestok = document.querySelector('.desktop-menu');
 const navbarEmail = document.querySelector('.navbar-email');
 
 navbarEmail.addEventListener("click", function() {
+   const isAsideclosed =  detallesProductos.classList.contains('inactive');
+   
+   if (!isAsideclosed){
+      detallesProductos.classList.add('inactive');
+   }
+
    menuDestok.classList.toggle('inactive');
   });
 
+
  //menu mobile
+
 
  const menuMobile = document.querySelector('.mobile-menu');
  const logoMenuMobile = document.querySelector('.menuLogo');
 
-
  logoMenuMobile.addEventListener("click", function() {
+   const isAsideclosed =  detallesProductos.classList.contains('inactive');
+   
+   if (!isAsideclosed){
+      detallesProductos.classList.add('inactive');
+   }
+
    menuMobile.classList.toggle('inactive');
  });
+
+
+ //carrito de compras (mostrar nuestros productos seleccionados)
+
+ 
+ const detallesProductos = document.querySelector('.product-detail');
+ const carChoppingIcon = document.querySelector('.navbar-shopping-cart');
+
+ carChoppingIcon.addEventListener("click", function(){
+   const isMobileMenuclosed =  menuMobile.classList.contains('inactive');
+   
+   if (!isMobileMenuclosed){
+      menuMobile.classList.add('inactive');
+   }
+  detallesProductos.classList.toggle('inactive');
+ })
